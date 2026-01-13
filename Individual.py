ed5f2f45eb3f3bb7db9e61b8b234e129165a2a4d
seraf_liv <6182301090@student.unpar.ac.id>
2026-01-13 12:53:55 +0700

    Edit individual agar memiliki fungsi evaluasi Terdapat 2 strategi berbeda untuk generate, yaitu w heuristik 4 dan tidak Random fill dipisah karena digunakan di strat1 dan strat2

--- Individual.py
+++ Individual.py
@@ -1,11 +1,16 @@
 import random
+from Fitness import Fitness
 
 # Individual adalah entitas yang merepresentasikan 1 solusi mosaic puzzle tertentu
 # @author  Serafina Livia Wardhana
+# Sumber kode -> Dokumen Mosaic, LLM 
 class Individual:
-    def __init__(self, size):
+    def __init__(self, board):
         # ukuran papan game mosaic
-        self.size = size
+        self.size = len(board)
+
+        # papan permainan yang berisi clue
+        self.board = board
 
         # kromosom berupa array 2d berukuran size*size
         # melambangkan pewarnaan papan
@@ -21,32 +26,45 @@ class Individual:
         # fungsi: agar clue yang sudah terselesaikan tidak di cek lagi
         self.solvedClues = set()
 
+        # inisialisasi fitness evaluator
+        self.fitnessEvaluator = Fitness(board)
+        
+        # variabel untuk menyimpan nilai fitness
+        self.fitness = 0
 
-    # method untuk menghasilkan kromosom pada satu individual
-    # solusi awal digenerate berdasarkan heuristik untuk mempercepat algoritma
-    # @param  board papan permainan yang berisi clue
-    def generate_chromosome(self, board):
+        # variabel untuk menyimpan pelanggaran pewarnaan papan
+        self.violation = 0
 
-        # warnai papan berdasarkan aturan heuristik 1-3 
-        self.fill_by_heu1to3(board)
 
-        # selain itu pewarnaan dilakukan secara acak dengan memerhatikan heuristik 4
-        self.fill_w_heu4(board)
+    # method untuk mengkalkulasi performa sebuah individu menggunakan fitnessEvaluator
+    def compute_fitness(self):
+        self.fitness, self.penalty, self.violation = self.fitnessEvaluator.compute_fitness(self)
+        return self.fitness
+    
+
+    # method untuk generate kromosom berdasarkan strategi heuristik 1-3 + random fill saja
+    def generate_chromosome_1stStrat(self):
+        self.fill_by_heu1to3(self.board)
+        self.fill_randomly(self.board)
+
+    # method untuk generate kromosom berdasarkan strategi heuristik 1-3 + random fill dengan memerhatikan heuristik 4
+    def generate_chromosome_2ndStrat(self):
+        self.fill_by_heu1to3(self.board)
+        self.fill_w_heu4(self.board)
 
 
     # method untuk melakukan pewarnaan berdasarkan heuristik 1-3 (penjelasan pada dokumen)
-    # @param  board papan permainan yang berisi clue
-    def fill_by_heu1to3(self, board):
+    def fill_by_heu1to3(self):
         for i in range(self.size):
             for j in range(self.size):
 
                 # jika pada sel tidak ada clue, skip
-                if board[i][j] == -1: 
+                if self.board[i][j] == -1: 
                     continue
 
                 # untuk clue di sudut papan
                 if i == 0 and j == 0:  # kiri atas
-                    if board[i][j] == 0:
+                    if self.board[i][j] == 0:
                         self.chromosome[i][j] = 0 # diri sendiri
                         self.chromosome[i][j+1] = 0  # kanan
                         self.chromosome[i+1][j] = 0  # bawah
@@ -57,7 +75,7 @@ class Individual:
                         self.certainCells.add((i+1, j+1))
                         self.solvedClues.add((i, j))
 
-                    elif board[i][j] == 4:
+                    elif self.board[i][j] == 4:
                         self.chromosome[i][j] = 1
                         self.chromosome[i][j+1] = 1 
                         self.chromosome[i+1][j] = 1  
@@ -69,7 +87,7 @@ class Individual:
                         self.solvedClues.add((i, j))  
                 
                 elif i == 0 and j == self.size - 1:  # kanan atas
-                    if board[i][j] == 0:
+                    if self.board[i][j] == 0:
                         self.chromosome[i][j] = 0 # diri sendiri
                         self.chromosome[i][j-1] = 0 # kiri
                         self.chromosome[i+1][j] = 0  # bawah
@@ -79,7 +97,7 @@ class Individual:
                         self.certainCells.add((i+1, j))
                         self.certainCells.add((i+1, j-1))
                         self.solvedClues.add((i, j))
-                    elif board[i][j] == 4:
+                    elif self.board[i][j] == 4:
                         self.chromosome[i][j] = 1
                         self.chromosome[i][j-1] = 1 
                         self.chromosome[i+1][j] = 1 
@@ -91,7 +109,7 @@ class Individual:
                         self.solvedClues.add((i, j))  
                 
                 elif i == self.size - 1 and j == 0:  # kiri bawah
-                    if board[i][j] == 0:
+                    if self.board[i][j] == 0:
                         self.chromosome[i][j] = 0 # diri sendiri
                         self.chromosome[i][j+1] = 0  # kanan
                         self.chromosome[i-1][j] = 0  # atas
@@ -101,7 +119,7 @@ class Individual:
                         self.certainCells.add((i-1, j))
                         self.certainCells.add((i-1, j+1))
                         self.solvedClues.add((i, j))
-                    elif board[i][j] == 4:
+                    elif self.board[i][j] == 4:
                         self.chromosome[i][j] = 1
                         self.chromosome[i][j+1] = 1  
                         self.chromosome[i-1][j] = 1  
@@ -113,7 +131,7 @@ class Individual:
                         self.solvedClues.add((i, j)) 
                 
                 elif i == self.size - 1 and j == self.size - 1:  # kanan bawah
-                    if board[i][j] == 0:
+                    if self.board[i][j] == 0:
                         self.chromosome[i][j] = 0 # diri sendiri
                         self.chromosome[i][j-1] = 0  # kiri
                         self.chromosome[i-1][j] = 0  # atas
@@ -123,7 +141,7 @@ class Individual:
                         self.certainCells.add((i-1, j))
                         self.certainCells.add((i-1, j-1))
                         self.solvedClues.add((i, j))
-                    elif board[i][j] == 4:
+                    elif self.board[i][j] == 4:
                         self.chromosome[i][j] = 1
                         self.chromosome[i][j-1] = 1  
                         self.chromosome[i-1][j] = 1  
@@ -137,7 +155,7 @@ class Individual:
 
                 # untuk clue di tepi papan (tidak termasuk sudut)
                 elif i == 0 and 0 < j < self.size - 1:  # tepi atas
-                    if board[i][j] == 0:
+                    if self.board[i][j] == 0:
                         self.chromosome[i][j] = 0 # diri sendiri
                         self.chromosome[i][j-1] = 0  # kiri
                         self.chromosome[i][j+1] = 0  # kanan
@@ -151,7 +169,7 @@ class Individual:
                         self.certainCells.add((i+1, j))
                         self.certainCells.add((i+1, j+1))
                         self.solvedClues.add((i, j))
-                    elif board[i][j] == 6:
+                    elif self.board[i][j] == 6:
                         self.chromosome[i][j] = 1
                         self.chromosome[i][j-1] = 1 
                         self.chromosome[i][j+1] = 1  
@@ -167,7 +185,7 @@ class Individual:
                         self.solvedClues.add((i, j))  
                 
                 elif i == self.size - 1 and 0 < j < self.size - 1:  # tepi bawah
-                    if board[i][j] == 0:
+                    if self.board[i][j] == 0:
                         self.chromosome[i][j] = 0 # diri sendiri
                         self.chromosome[i][j-1] = 0 # kiri
                         self.chromosome[i][j+1] = 0  # kanan
@@ -181,7 +199,7 @@ class Individual:
                         self.certainCells.add((i-1, j))
                         self.certainCells.add((i-1, j+1))
                         self.solvedClues.add((i, j))
-                    elif board[i][j] == 6:
+                    elif self.board[i][j] == 6:
                         self.chromosome[i][j] = 1
                         self.chromosome[i][j-1] = 1  
                         self.chromosome[i][j+1] = 1  
@@ -197,7 +215,7 @@ class Individual:
                         self.solvedClues.add((i, j))  
                 
                 elif j == 0 and 0 < i < self.size - 1:  # tepi kiri
-                    if board[i][j] == 0:
+                    if self.board[i][j] == 0:
                         self.chromosome[i][j] = 0
                         self.chromosome[i-1][j] = 0  # atas
                         self.chromosome[i+1][j] = 0  # bawag
@@ -211,7 +229,7 @@ class Individual:
                         self.certainCells.add((i, j+1))
                         self.certainCells.add((i+1, j+1))
                         self.solvedClues.add((i, j))
-                    elif board[i][j] == 6:
+                    elif self.board[i][j] == 6:
                         self.chromosome[i][j] = 1
                         self.chromosome[i-1][j] = 1 
                         self.chromosome[i+1][j] = 1 
@@ -227,7 +245,7 @@ class Individual:
                         self.solvedClues.add((i, j)) 
                 
                 elif j == self.size - 1 and 0 < i < self.size - 1:  # tepi kanan
-                    if board[i][j] == 0:
+                    if self.board[i][j] == 0:
                         self.chromosome[i][j] = 0 # diri sendiri
                         self.chromosome[i-1][j] = 0  # atas
                         self.chromosome[i+1][j] = 0  # bawah
@@ -241,7 +259,7 @@ class Individual:
                         self.certainCells.add((i, j-1))
                         self.certainCells.add((i+1, j-1))
                         self.solvedClues.add((i, j))
-                    elif board[i][j] == 6:
+                    elif self.board[i][j] == 6:
                         self.chromosome[i][j] = 1 # diri 
                         self.chromosome[i-1][j] = 1  # atas
                         self.chromosome[i+1][j] = 1  # bawah
@@ -259,7 +277,7 @@ class Individual:
 
                 # untuk clue yang tidak berada tepi maupun sudut
                 else:  
-                    if board[i][j] == 0:
+                    if self.board[i][j] == 0:
                         self.chromosome[i][j] = 0  # diri sendiri
                         self.chromosome[i-1][j-1] = 0  # atas kiri
                         self.chromosome[i-1][j] = 0  # atas
@@ -279,7 +297,7 @@ class Individual:
                         self.certainCells.add((i+1, j))
                         self.certainCells.add((i+1, j+1))
                         self.solvedClues.add((i, j))
-                    elif board[i][j] == 9:
+                    elif self.board[i][j] == 9:
                         self.chromosome[i][j] = 1  
                         self.chromosome[i-1][j-1] = 1 
                         self.chromosome[i-1][j] = 1 
@@ -299,10 +317,24 @@ class Individual:
                         self.certainCells.add((i+1, j))
                         self.certainCells.add((i+1, j+1))
                         self.solvedClues.add((i, j))  
-                
+
+    # method untuk melakukan pewarnaan secara acak pada clue yang belum terselesaikan
+    def fill_randomly(self):
+        for i in range(self.size):
+            for j in range(self.size):
+                if (i, j) not in self.certainCells:
+                    # cek apakah sel berada di sekitar clue unsolved
+                    neighbors = self.get_neighbors(i, j)
+                    has_unsolved_clue = any(
+                        self.board[ni][nj] != -1 and (ni, nj) not in self.solvedClues # true jika ada minimal 1 tetangga yang yang berupa clue (bukan -1/kosong) dan tidak termasuk di solvedClues
+                        for ni, nj in neighbors
+                    )
+                    
+                    if has_unsolved_clue:
+                        self.chromosome[i][j] = random.randint(0, 1)
+         
     # method untuk melakukan pewarnaan berdasarkan heuristik 4 (penjelasan pada dokumen)
-    # @param board  papan permainan yang berisi clue
-    def fill_w_heu4(self, board):
+    def fill_w_heu4(self):
         # sel discan secara iteratif, sel harus memiliki nilai isCertain FALSE
         # cek apakah elligible untuk heuristik 4 sebagai berikut
 
@@ -313,27 +345,27 @@ class Individual:
             # update solvedClues berdasarkan data terbaru
             for i in range(self.size):
                 for j in range(self.size):
-                    if board[i][j] == -1:  # skip jika bukan clue
+                    if self.board[i][j] == -1:  # skip jika bukan clue
                         continue
                     
                     neighbors = self.get_neighbors(i, j)
                     # cek apakah semua neighbors sudah pasti
-                    all_determined = all((ni, nj) in self.certainCells for ni, nj in neighbors)
+                    allDetermined = all((ni, nj) in self.certainCells for ni, nj in neighbors)
                     
-                    if all_determined:
+                    if allDetermined:
                         self.solvedClues.add((i, j))
             
 
-            # scan semua sel pada board untuk mencari clue 
+            # scan semua sel pada self.board untuk mencari clue 
             for i in range(self.size):
                 for j in range(self.size):
-                    if board[i][j] == -1:  # skip jika bukan clue 
+                    if self.board[i][j] == -1:  # skip jika bukan clue 
                         continue
                     
                     if (i, j) in self.solvedClues: # skip clue yang sudah solved
                         continue
                     
-                    clue = board[i][j]
+                    clue = self.board[i][j]
                     neighbors = self.get_neighbors(i, j) # cari koordinat tetangga
                     
                     black_count = 0
@@ -358,7 +390,7 @@ class Individual:
                         needed_black = clue - black_count
                         validCandidates = []
                         for ni, nj in neighbors: # cari kandidat valid dari sel kosong
-                            if (ni, nj) not in self.certainCells and self.is_valid_if_black(ni, nj, board):
+                            if (ni, nj) not in self.certainCells and self.is_valid_if_black(ni, nj, self.board):
                                 validCandidates.append((ni, nj))
                         
                         # jika jumlah sel kosong sama dengan jumlah sel hitam yang dibutuhkan, sel kosong tersebut pasti hitam
@@ -367,28 +399,16 @@ class Individual:
                                 self.chromosome[ni][nj] = 1
                                 self.certainCells.add((ni, nj))
                                 isAnyChangesCanBeMade = True
-
+               
             # random fill (hanya untuk sel not certain yang berada di sekitar clue unsolved)                 
-            for i in range(self.size):
-                for j in range(self.size):
-                    if (i, j) not in self.certainCells:
-                        # cek apakah sel berada di sekitar clue unsolved
-                        neighbors = self.get_neighbors(i, j)
-                        has_unsolved_clue = any(
-                            board[ni][nj] != -1 and (ni, nj) not in self.solvedClues # true jika ada minimal 1 tetangga yang yang berupa clue (bukan -1/kosong) dan tidak termasuk di solvedClues
-                            for ni, nj in neighbors
-                        )
-                        
-                        if has_unsolved_clue:
-                            self.chromosome[i][j] = random.randint(0, 1)
-
+            self.fill_randomly(self.board)
 
             # jika penandaan sel sebagai hitam melanggar constraint, tandai sebagai putih
             for i in range(self.size):
                 for j in range(self.size):
                     if (i, j) not in self.certainCells:
                         # cek apakah pengisian sel valid
-                        if not self.is_valid_if_black(i, j, board):
+                        if not self.is_valid_if_black(i, j, self.board):
                             self.chromosome[i][j] = 0
                             self.certainCells.add((i, j))
                             isAnyChangesCanBeMade = True
@@ -410,14 +430,14 @@ class Individual:
     # method untuk mengecek apakah pewarnaan sel hitam pada sel (i,j) melanggar constraint
     # @param i  nomor baris yang ditempati sel target
     # @param j  nomor kolom yang ditempati sel target
-    def is_valid_if_black(self, i, j, board):
+    def is_valid_if_black(self, i, j):
         neighbors = self.get_neighbors(i, j)
         
         for ni, nj in neighbors: 
-            if board[ni][nj] == -1: # cari tetangga target yang merupakan clue
+            if self.board[ni][nj] == -1: # cari tetangga target yang merupakan clue
                 continue
             
-            clue = board[ni][nj]
+            clue = self.board[ni][nj]
             neighbor_neighbors = self.get_neighbors(ni, nj) # cari koordinat tetangga dari tetangga
             
             # hitung berapa sel hitam yang ada jika (i,j) diset hitam 
