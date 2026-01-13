07e991f2a4858fac7ea64cefa48810d08f4eb0ca
seraf_liv <6182301090@student.unpar.ac.id>
2026-01-13 12:51:12 +0700

    Fitness dipisah jadi kelas sendiri

--- Fitness.py
+++ Fitness.py
@@ -1,44 +1,60 @@
 import math
 
+# Class ini digunakan untuk mengevaluasi fitness dari solusi individual
+# @author  Nadhira Saffanah Zahra
 #Sumber kode -> Dokumen Mosaic, LLM 
+class Fitness:
+    def __init__(self, board):
+        # ukuran papan game mosaic
+        self.size = len(board)
 
-# count black box : hitung jumlah kotak hitam sebenarnya pada papan individual 
-def count_black_box(chromosome, x, y, size):
-    count = 0
+        # papan permainan yang berisi clue
+        self.board = board
+
+
+    # fungsi untuk menghitung penalti dari solusi individual
+    def compute_fitness(self, individual):
+        fitness = 0
+        penalty = self.count_penalty(individual, self.board)
+
+        # hitung fitness 
+        fitness = 1 / (1 + penalty)
+        return fitness, penalty
+
+    # fungsi untuk menghitung penalty pada setiap individu 
+    def count_penalty(self, individual):
+        penalty = 0 
+        size = individual.size
+
+        # lakukan loop untuk baris dan kolom individual 
+        for i in range(size):
+            for j in range(size): 
+                # jika pada sel tidak ada clue, skip
+                if self.board[i][j] == -1:
+                    continue
+
+                # ambil nilai jumlah kotak hitam seharusnya 
+                expected_black_box = self.board[i][j]
+
+                # ambil jumlah kotak hitam sebenarnya pada papan individual
+                actual_black_box = self.count_black_box(individual.chromosome, i, j)
+
+                # hitung penalti berdasarkan selisih antara expected dan actual 
+                penalty += abs(expected_black_box - actual_black_box)
+
+        return penalty 
     
-    #masih belom paham 
-    for dx in [-1, 0, 1]:
-        for dy in [-1, 0, 1]:
-            nx, ny = x + dx, y + dy
-            if 0 <= nx < size and 0 <= ny < size:
-                count += chromosome[nx][ny]
-    return count
-
-# fitness function: hitung penalti dari solusi individual   
-def compute_fitness(individual, board):
-    penalty = 0
-    fitness = 0
-    size = individual.size()
-
-    # Lakukan loop untuk baris dan kolom individual 
-    for i in range(size):
-        for j in range(size): 
-            # jika pada sel tidak ada clue, skip
-            if board[i][j] == -1:
-                continue
-
-            # ambil nilai jumlah kotak hitam seharusnya 
-            expected_black_box = board[i][j]
-
-            # ambil jumlah kotak hitam sebenarnya pada papan individual
-            actual_black_box = count_black_box(individual.chromosome, i, j, size)
-
-            # hitung penalti berdasarkan selisih antara expected dan actual 
-            penalty += abs(expected_black_box - actual_black_box)
-
-    # hitung fitness 
-    fitness = 1 / (1 + penalty)
-    return fitness
+    # count_black_box : hitung jumlah kotak hitam sebenarnya pada papan individual 
+    def count_black_box(self, chromosome, x, y):
+        count = 0
+        
+        #masih belom paham 
+        for dx in [-1, 0, 1]:
+            for dy in [-1, 0, 1]:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < self.size and 0 <= ny < self.size:
+                    count += chromosome[nx][ny]
+        return count
 
 
-
+  
